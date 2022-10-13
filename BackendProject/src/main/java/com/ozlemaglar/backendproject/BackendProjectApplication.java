39615d498723f22dbaebb5e443cf7228d55876f9
ozlemsoydan <71570727+ozlemsoydan@users.noreply.github.com>
2022-10-13 16:22:42 +0300

    libraries created

--- BackendProject/src/main/java/com/ozlemaglar/backendproject/BackendProjectApplication.java
+++ BackendProject/src/main/java/com/ozlemaglar/backendproject/BackendProjectApplication.java
@@ -1,4 +1,4 @@
-package com.ozlemsoydan.backendproject;
+package com.ozlemaglar.backendproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendProjectApplication {
 
     public static void main(String[] args) {
+        System.setProperty("spring.devtools.restart.enabled", "true");
+        System.setProperty("java.awt.headless", "false"); //Disables headless
         SpringApplication.run(BackendProjectApplication.class, args);
     }
 
