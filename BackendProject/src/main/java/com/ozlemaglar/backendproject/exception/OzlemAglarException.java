d447599a957f6b3d8d829590b2b5648a4f0a5fa8
ozlemsoydan <71570727+ozlemsoydan@users.noreply.github.com>
2022-10-13 17:28:16 +0300

    exception

---
+++ BackendProject/src/main/java/com/ozlemaglar/backendproject/exception/OzlemAglarException.java
@@ -0,0 +1,8 @@
+package com.ozlemaglar.backendproject.exception;
+
+public class OzlemAglarException extends RuntimeException{
+
+    public OzlemAglarException(String message) {
+        super(message);
+    }
+}
---
+++ BackendProject/src/main/java/com/ozlemaglar/backendproject/exception/ResourceNotFoundException.java
@@ -0,0 +1,13 @@
+package com.ozlemaglar.backendproject.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+//404
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
