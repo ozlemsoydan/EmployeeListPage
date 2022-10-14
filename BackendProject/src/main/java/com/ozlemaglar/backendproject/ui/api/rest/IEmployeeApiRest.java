a344222c72d147b19b8837a4cce3cb56620ec0e5
ozlemsoydan <71570727+ozlemsoydan@users.noreply.github.com>
2022-10-14 15:17:55 +0300

    rest

---
+++ BackendProject/src/main/java/com/ozlemaglar/backendproject/ui/api/rest/IEmployeeApiRest.java
@@ -0,0 +1,29 @@
+package com.ozlemaglar.backendproject.ui.api.rest;
+
+import com.ozlemaglar.backendproject.business.dto.EmployeeDto;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.Map;
+
+public interface IEmployeeApiRest {
+
+
+    public String getRoot();
+
+    //save
+    public ResponseEntity<EmployeeDto>  createEmployee(EmployeeDto employeeDto);
+
+    //list
+    public ResponseEntity<List<EmployeeDto>>  getAllEmployees();
+
+    //find
+    public ResponseEntity<EmployeeDto> getEmployeeById(Long id);
+
+    //update
+    public ResponseEntity<EmployeeDto>  updateEmployee(Long id, EmployeeDto employeeDto);
+
+    //delete
+    public ResponseEntity<Map<String,Boolean>>  deleteEmployee(Long id);
+}
+
---
+++ BackendProject/src/main/java/com/ozlemaglar/backendproject/ui/api/rest/controller/EmployeeRestApi.java
@@ -0,0 +1,84 @@
+package com.ozlemaglar.backendproject.ui.api.rest.controller;
+
+
+import com.ozlemaglar.backendproject.business.dto.EmployeeDto;
+import com.ozlemaglar.backendproject.business.service.IEmployeeService;
+import com.ozlemaglar.backendproject.ui.api.rest.IEmployeeApiRest;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.PackagePrivate;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Log4j2
+@RequiredArgsConstructor
+
+@RestController
+@RequestMapping("/employee/api/v1")
+@CrossOrigin(origins = "http://localhost:3000")
+
+public class EmployeeRestApi implements IEmployeeApiRest {
+
+
+    private final IEmployeeService service;
+
+    //http://localhost:8080/employee/api/v1
+    //http://localhost:8080/employee/api/v1/index
+    @Override
+    @GetMapping({"/","/index"})
+    public String getRoot() {
+        return null;
+    }
+
+    //Save
+    //http://localhost:8080/employee/api/v1/employees
+    @Override
+    @PostMapping("/employees")
+    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto) {
+        service.createEmployee(employeeDto);
+        return ResponseEntity.ok(employeeDto);
+    }
+
+
+    //List
+    //http://localhost:8080/employee/api/v1/employees
+    @Override
+    @GetMapping("/employees")
+    public ResponseEntity<List<EmployeeDto>> getAllEmployees() {
+        List<EmployeeDto> list =service.getAllEmployees();
+        return ResponseEntity.ok(list);
+    }
+
+    //Find
+    //http://localhost:8080/employee/api/v1/employees/1
+    @Override
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable(name="id") Long id) {
+        return ResponseEntity.ok(service.getEmployeeById(id));
+    }
+
+
+    //Update
+    //
+    @Override
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<EmployeeDto> updateEmployee(Long id, EmployeeDto employeeDto) {
+        service.updateEmployee(id,employeeDto);
+        return ResponseEntity.ok(employeeDto);
+    }
+
+    //Delete
+    //http://localhost:8080/employee/api/v1/employees/1
+    @Override
+    @DeleteMapping("/employees/{id}")
+    public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable(name="id") Long id) {
+        service.deleteEmployee(id);
+        Map<String,Boolean> response=new HashMap<>();
+        response.put("silindi", Boolean.TRUE);
+        return ResponseEntity.ok(response);
+    }
+}
