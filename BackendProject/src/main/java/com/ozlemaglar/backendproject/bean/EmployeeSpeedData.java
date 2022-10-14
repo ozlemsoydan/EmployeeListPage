0314b451437acdbfc44977ac30c8c376b0bbca76
ozlemsoydan <71570727+ozlemsoydan@users.noreply.github.com>
2022-10-14 17:55:52 +0300

    api configuration

--- BackendProject/src/main/java/com/ozlemaglar/backendproject/bean/EmployeeSpeedData.java
+++ BackendProject/src/main/java/com/ozlemaglar/backendproject/bean/EmployeeSpeedData.java
@@ -8,18 +8,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.CommonDataSource;
+import java.util.UUID;
 
 @Configuration
 public class EmployeeSpeedData {
+
 
     @Bean
     CommandLineRunner createEmployeeData(EmployeeServiceImpl service){
         return (args)->{
             for (int i = 1; i <5 ; i++) {
+                UUID uuid= UUID.randomUUID();
                 EmployeeDto employeeDto = EmployeeDto.builder()
-                        .email("ozm.sydn@gmail.com"+i)
+                        .email(uuid+ "@gmail.com")
                         .password("root"+i)
-                        .username("özlem ağlar"+i).build();
+                        .username("özlem ağlar"+i)
+                        .price(10*i).build();
                 service.createEmployee(employeeDto);
             }
 
---
+++ BackendProject/src/main/java/com/ozlemaglar/backendproject/persistances/Postman.java
@@ -0,0 +1,27 @@
+package com.ozlemaglar.backendproject.persistances;
+
+public class Postman {
+
+
+    //SAVE
+    //POST
+    //http://localhost:8080/employee/api/v1/employees
+
+
+    //LIST
+    //GET
+    //http://localhost:8080/employee/api/v1/employees
+
+    //FIND
+    //GET
+    //http://localhost:8080/employee/api/v1/employees/1
+
+
+    //DELETE
+    //http://localhost:8080/employee/api/v1/employees/1
+
+
+    //UPDATE
+    //http://localhost:8080/employee/api/v1/employees/1
+
+}
