cc0fd0b696bc7cc3936dd11ef30e2e6337ce0120
ozlemsoydan <71570727+ozlemsoydan@users.noreply.github.com>
2022-10-16 01:24:27 +0300

    components

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Main from './components/Main';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <>
-    <App />
+    <Header/>
+    <Main/>
+    <Footer/>
     </>
     
   </React.StrictMode>
