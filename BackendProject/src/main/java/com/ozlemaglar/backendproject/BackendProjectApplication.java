b213ba8c813e0bfed046f8cb8543bc2876ea9004
ozlemsoydan <71570727+ozlemsoydan@users.noreply.github.com>
2022-10-15 21:50:29 +0300

    PostConstruct

--- BackendProject/src/main/java/com/ozlemaglar/backendproject/BackendProjectApplication.java
+++ BackendProject/src/main/java/com/ozlemaglar/backendproject/BackendProjectApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 
 //Auditor
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
@@ -18,6 +21,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 //@SpringBootApplication
 public class BackendProjectApplication {
 
+    @PostConstruct
+    public void init(){
+        TimeZone.setDefault(TimeZone.getTimeZone("IST"));
+
+    }
     public static void main(String[] args) {
         System.setProperty("spring.devtools.restart.enabled", "true");
         System.setProperty("java.awt.headless", "false"); //Disables headless
