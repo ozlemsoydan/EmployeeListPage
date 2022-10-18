e535ab97188d0d0621fb13d17bb0f3b8035c8b5b
ozlemsoydan <71570727+ozlemsoydan@users.noreply.github.com>
2022-10-18 19:35:05 +0300

    react-router-dom

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,19 +3,34 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 import reportWebVitals from './reportWebVitals';
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Main from './components/Main';
+
+import EmloyeeList from './components/EmloyeeList';
+import EmployeeCreate from './components/EmployeeCreate';
+import EmployeeView from './components/EmployeeView';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <>
-    <Header/>
-    <Main/>
-    <Footer/>
+      <Router>
+        <Header />
+        <div className="container">
+          <Switch>
+            <Route path='/' exact component={EmloyeeList}></Route>
+            <Route path='/employees' exact component={EmloyeeList}></Route>
+            <Route path='/employee-add/:id' exact component={EmployeeCreate}></Route>
+            <Route path='employee-view/:id' exact component={EmployeeView}></Route>
+          </Switch>
+        </div>
+
+        <Footer />
+      </Router>
     </>
-    
+
   </React.StrictMode>
 );
 
