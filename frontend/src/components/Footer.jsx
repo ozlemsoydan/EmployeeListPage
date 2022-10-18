5ed95fc254a60e232b38f3aec0e1b54a8d864cca
ozlemsoydan <71570727+ozlemsoydan@users.noreply.github.com>
2022-10-18 19:34:23 +0300

    footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,6 +2,61 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <>
+      <footer className="text-center text-white " >
+
+        <div className="container pt-4">
+
+          <section className="mb-4">
+            <a className="btn btn-link btn-floating btn-lg text-dark m-1"
+              href="#!"
+              role="button"
+              data-mdb-ripple-color="dark">
+              <i className="fab fa-facebook-f"></i>
+            </a>
+
+            <a className="btn btn-link btn-floating btn-lg text-dark m-1"
+              href="#!"
+              role="button"
+              data-mdb-ripple-color="dark">
+              <i className="fab fa-twitter"></i>
+            </a>
+
+            <a className="btn btn-link btn-floating btn-lg text-dark m-1"
+              href="#!"
+              role="button"
+              data-mdb-ripple-color="dark">
+              <i className="fab fa-google"></i>
+            </a>
+
+            <a className="btn btn-link btn-floating btn-lg text-dark m-1"
+              href="#!"
+              role="button"
+              data-mdb-ripple-color="dark">
+              <i className="fab fa-instagram"></i>
+            </a>
+
+            <a className="btn btn-link btn-floating btn-lg text-dark m-1"
+              href="#!"
+              role="button"
+              data-mdb-ripple-color="dark">
+              <i className="fab fa-linkedin"></i>
+            </a>
+            <a className="btn btn-link btn-floating btn-lg text-dark m-1"
+              href="#!"
+              role="button"
+              data-mdb-ripple-color="dark">
+              <i className="fab fa-github"></i>
+            </a>
+          </section>
+        </div>
+
+        <div className="text-center text-dark p-3" >
+          © 2020 Copyright:
+          <a className="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+        </div>
+      </footer>
+
+    </>
   )
 }
